18f2adea67832358f098512f1bf0bd2dda9bf6c9
normalmn <appliedboredom@gmail.com>
2019-12-15 21:53:03 -0600

    making files, pseudocoding JS

---
+++ hangman.js
@@ -0,0 +1,26 @@
+//initialize global variables: 
+    //lives
+    //current letter of word 
+    //wins
+    //losses
+    //word list
+    
+
+//word list array
+
+//pick word from list
+
+//render word in HTML as blanks
+
+//get user input for letter
+
+//check user input against current letter
+
+//if user input === letter, advance to next letter, and wait for new input
+
+//else, if user input !== letter, log a miss, lose a life, don't advance, and wait for new input
+
+//if user gets full word complete, show win screen
+
+//if user loses all lives, show loss screen
+
